47cb9d57d5b78a2ffef06ffeb40b0ff1c6c570af
yamaryu2405 <149277325+yamaryu2405@users.noreply.github.com>
2024-04-15 14:53:42 +0900

    kadai_011を修正

--- kadai_011/kadai_011.py
+++ kadai_011/kadai_011.py
@@ -3,8 +3,9 @@ array = ["水","金","地","火","木","土","天","海","冥"]
 for value in array:
 			print(value)
 
-value =""
 
-while  value!="冥":
-		for value in array:
-			print(value)
+count =0
+
+while  count < 8:
+	print(array[count])
+	count +=1
